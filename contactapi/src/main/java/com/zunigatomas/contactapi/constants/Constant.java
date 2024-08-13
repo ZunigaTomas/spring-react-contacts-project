b7dbc940a2e7f1ac2c6296e62057e8d69e859c2e
ZunigaTomas <zuniga.tomas99@gmail.com>
2024-08-12 23:00:24 -0300

    Agregué configuraciones del CORS

--- contactapi/src/main/java/com/zunigatomas/contactapi/constants/Constant.java
+++ contactapi/src/main/java/com/zunigatomas/contactapi/constants/Constant.java
@@ -2,4 +2,5 @@ package com.zunigatomas.contactapi.constants;
 
 public class Constant {
     public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads";
+    public static final String X_REQUESTED_WITH = "X-Requested-With";
 }
